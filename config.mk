aa892a29d80d70ef7b06c9b5e1b4647a3bc1308f
dleins <dleins@techfak.uni-bielefeld.de>
2019-01-26 20:49:07 +0100

    Add first customizations

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 6.1
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /home/spectre/suckless-tools/install/dwm
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include
+X11LIB = /usr/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
