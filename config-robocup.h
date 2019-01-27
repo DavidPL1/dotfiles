ce0396a9412f7f4dd3109e58abfb3c25d6e4fe62
dleins <dleins@techfak.uni-bielefeld.de>
2019-01-27 14:21:51 +0100

    Add more window config for robocup profile

--- config-robocup.h
+++ config-robocup.h
@@ -29,18 +29,29 @@ static const char *colors[][3] = {
 /* some cool icons:
  *                                                                      
  */
-static const char *tags[] = { "", "", "", "Rviz", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "Rviz", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-    {  "TelegramDesktop",     NULL,       NULL,         1 << 5,            0,           -1 },
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "de.unibi.citec.clf.bonsai.engine.fxgui.FXGUI",  NULL,       NULL,       NULL,       1,           -1 },
+	/* class                                            instance    title       tags mask     isfloating   monitor */
+	{ "Firefox",                                        NULL,       NULL,       1 << 8,       0,           -1 },
+
+
+    { "tbrains-pycharm-ce",         "sun-awt-X11-XFramePeer",       NULL,       1 << 6,       0,           -1 },
+    { "jetbrains-idea-ce",          "sun-awt-X11-XFramePeer",       NULL,       1 << 7,       0,           -1 },
+    { "XTerm",                                          NULL,       NULL,       NULL,         1,           -1 },
+    { NULL,                                             NULL,       "cmd_vel",  1 << 3,       1,           -1 },
+    { NULL,           NULL,       "rosrun keyboard_teleop keyboard_teleop.py ", 1 << 3,       1,           -1 },
+    { "rviz",                                           NULL,       NULL,       1 << 3,       0,           -1 },
+    { "rqt_gui",               NULL,       "tiago_control.perspective - rqt",   1 << 3,       1,           -1 },
+    { "gazeb",                                          NULL,       "Gazebo",   1 << 2,       0,           -1 },
+    { "Vdemo2_controller.tcl",                          NULL,       NULL,       1 << 1,       1,           -1 },
+    { "Thunderbird",                                    NULL,       NULL,       1 << 5,       0,           -1 },
+    {  "TelegramDesktop",                               NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "de.unibi.citec.clf.bonsai.engine.fxgui.FXGUI",   NULL,       NULL,       NULL,         1,           -1 },
 };
 
 /* layout(s) */
