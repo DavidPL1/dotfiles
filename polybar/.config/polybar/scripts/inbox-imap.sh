9c4f21823e228aa7740cd6e17bb3a5c96f28ecf3
dleins <dleins@techfak.uni-bielefeld.de>
2019-10-26 12:55:42 +0200

    Updated polybar with colors and a few newer modules

---
+++ polybar/.config/polybar/scripts/inbox-imap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+NETRC="/home/spectre/.netrc"
+
+inbox=$(curl -sf --netrc-file "$NETRC" -X "STATUS INBOX (UNSEEN)" imaps://"$SERVER"/INBOX | tr -d -c "[:digit:]")
+
+if [ "$inbox" ] && [ "$inbox" -gt 0 ]; then
+    echo " $inbox"
+else
+    echo ""
+fi
