1994aa459d8054703bcbd7ce3aad4fa63e534aa3
dleins <dleins@techfak.uni-bielefeld.de>
2020-06-08 14:51:50 +0200

    Some refactoring

--- screenlayout/.screenlayout/auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env sh
-
-if type "xrandr"; then
-    if xrandr --query | grep " connected" | grep "VGA1"; then
-        /bin/sh $HOME/.screenlayout/home.sh
-    else
-        /bin/sh $HOME/.screenlayout/laptop-only.sh
-    fi
-fi
